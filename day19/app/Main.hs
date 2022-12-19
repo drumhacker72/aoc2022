287d75efc8e3c847d22ae297206183b3dfbf0dbc
J. Nelson <drumhacker72@gmail.com>
2022-12-19 10:25:55 -0600

    day 19, part 2 (much less terrible solution)

--- day19/app/Main.hs
+++ day19/app/Main.hs
@@ -1,13 +1,11 @@
-import Control.Monad (forM_)
 import Data.Bifunctor (first, second)
 import Data.Functor (void)
 import Data.List (foldl')
-import Data.Map.Strict (Map)
+import Data.Maybe (fromJust, maybeToList)
 import Data.Void (Void)
 import Text.Megaparsec (Parsec, (<|>), parseMaybe, sepBy1, sepEndBy1, some)
 import Text.Megaparsec.Char (char, space1, string)
 
-import qualified Data.Map.Strict as M
 import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void String
@@ -18,12 +16,12 @@ data Resource = Clay | Geode | Obsidian | Ore
 data ResourceMap a = ResourceMap !a !a !a !a
     deriving Show
 
-fromList :: [(Resource, Int)] -> ResourceMap Int
-fromList [] = ResourceMap 0 0 0 0
-fromList ((Clay, a):xs) = let ResourceMap _ b c d = fromList xs in ResourceMap a b c d
-fromList ((Geode, b):xs) = let ResourceMap a _ c d = fromList xs in ResourceMap a b c d
-fromList ((Obsidian, c):xs) = let ResourceMap a b _ d = fromList xs in ResourceMap a b c d
-fromList ((Ore, d):xs) = let ResourceMap a b c _ = fromList xs in ResourceMap a b c d
+fromList :: a -> [(Resource, a)] -> ResourceMap a
+fromList e [] = ResourceMap e e e e
+fromList e ((Clay, a):xs) = let ResourceMap _ b c d = fromList e xs in ResourceMap a b c d
+fromList e ((Geode, b):xs) = let ResourceMap a _ c d = fromList e xs in ResourceMap a b c d
+fromList e ((Obsidian, c):xs) = let ResourceMap a b _ d = fromList e xs in ResourceMap a b c d
+fromList e ((Ore, d):xs) = let ResourceMap a b c _ = fromList e xs in ResourceMap a b c d
 
 adjust :: (a -> a) -> Resource -> ResourceMap a -> ResourceMap a
 adjust f k (ResourceMap a b c d) = case k of
@@ -44,6 +42,14 @@ rsrcZipWith f (ResourceMap a1 b1 c1 d1) (ResourceMap a2 b2 c2 d2) = ResourceMap 
 
 rsrcFoldr :: (a -> b -> b) -> b -> ResourceMap a -> b
 rsrcFoldr f x (ResourceMap a b c d) = f a (f b (f c (f d x)))
+
+rsrcSequence :: Monad m => ResourceMap (m a) -> m (ResourceMap a)
+rsrcSequence (ResourceMap a b c d) = do
+    a' <- a
+    b' <- b
+    c' <- c
+    d' <- d
+    return $ ResourceMap a' b' c' d'
 
 pResource :: Parser Resource
 pResource =
@@ -68,9 +74,9 @@ pRecipe = do
     void $ string " robot costs "
     ingredients <- sepBy1 pResourceAmount (string " and ")
     void $ char '.'
-    pure (robot, fromList ingredients)
+    pure (robot, fromList 0 ingredients)
 
-type Blueprint = Map Resource (ResourceMap Int)
+type Blueprint = ResourceMap (ResourceMap Int)
 
 pBlueprint :: Parser (Int, Blueprint)
 pBlueprint = do
@@ -79,7 +85,7 @@ pBlueprint = do
     void $ char ':'
     space1
     recipes <- sepEndBy1 pRecipe space1
-    pure (idNum, M.fromList recipes)
+    pure (idNum, fromJust $ rsrcSequence $ fromList Nothing (second Just <$> recipes))
 
 data State = State
     { resources :: !(ResourceMap Int)
@@ -89,9 +95,9 @@ data State = State
 gather :: State -> ResourceMap Int
 gather (State rs bs) = rsrcZipWith (+) rs bs
 
-tryMakeRobot :: (Resource, ResourceMap Int) -> State -> [(State, ResourceMap Int)]
+tryMakeRobot :: (Resource, ResourceMap Int) -> State -> Maybe (State, ResourceMap Int)
 tryMakeRobot (robot, ingredients) s@(State rs bs) =
-    if rsrcFoldr (&&) True (rsrcZipWith (>=) rs ingredients) then [(s', bs')] else []
+    if rsrcFoldr (&&) True (rsrcZipWith (>=) rs ingredients) then Just (s', bs') else Nothing
   where
     s' = s { resources = rsrcZipWith (-) rs ingredients }
     bs' = adjust (+ 1) robot bs
@@ -100,20 +106,23 @@ membersGTE :: ResourceMap Int -> ResourceMap Int -> Bool
 membersGTE (ResourceMap a1 b1 c1 d1) (ResourceMap a2 b2 c2 d2) = a1 >= a2 && b1 >= b2 && c1 >= c2 && d1 >= d2
 
 isBetterThan :: Int -> Blueprint -> State -> State -> Bool
-isBetterThan t bp (State r1 b1) (State r2 b2) =
-    (r1 `membersGTE` r2 && b1 `membersGTE` b2)
-    || (r1 ! Obsidian + t * (b1 ! Obsidian) > r2 ! Obsidian + t * (b2 ! Obsidian)
-        && r1 ! Geode + t * (b1 ! Geode) > r2 ! Geode + t * (b2 ! Geode))
+isBetterThan _t _bp (State r1 b1) (State r2 b2) = r1 `membersGTE` r2 && b1 `membersGTE` b2
 
 step :: Int -> Blueprint -> [State] -> [State]
 step t bp states =
-    let builds = concatMap (\s@(State _ bs) -> (s, bs) : concatMap (\r -> tryMakeRobot r s) (M.assocs bp)) states
+    let builds = concatMap (\s@(State _ bs) ->
+            case tryMakeRobot (Geode, bp ! Geode) s of
+                Just x -> [x]
+                Nothing -> case tryMakeRobot (Obsidian, bp ! Obsidian) s of
+                    Just x -> [x]
+                    Nothing -> (s, bs) : concatMap (\r -> maybeToList $ tryMakeRobot (r, bp ! r) s) [Clay, Ore]
+            ) states
         gathers = map (uncurry State . first gather) builds
      in foldl' (\acc s -> (if any (\x -> isBetterThan t bp x s) acc then filter (not . (isBetterThan t bp s)) acc else s:acc))
                [] gathers
 
 loop :: Int -> Blueprint -> [State] -> [[State]]
-loop 0 _ states = []
+loop 0 _ _ = []
 loop t bp states = let states' = step t bp states in states' : loop (t-1) bp states'
 
 numGeodes :: State -> Int
@@ -123,9 +132,9 @@ main :: IO ()
 main = do
     contents <- readFile "day19.txt"
     let blueprints = maybe (error "bad input") id $ parseMaybe (some pBlueprint) contents
-    let initial = State (fromList []) (fromList [(Ore, 1)])
-
-    -- forM_ blueprints $ \(_, bp) ->  mapM (print . second length) $ zip [1::Int ..] $ loop 24 bp [initial]
+    let initial = State (fromList 0 []) (fromList 0 [(Ore, 1)])
 
     let qualityLevels = map (\(idNum, bp) -> idNum * maximum (map numGeodes $ last $ loop 24 bp [initial])) blueprints
     print $ sum qualityLevels
+
+    print $ product $ map (\(_, bp) -> maximum (map numGeodes $ last $ loop 32 bp [initial])) $ take 3 blueprints
