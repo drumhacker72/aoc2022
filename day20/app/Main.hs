d333820195b731b6ec301c5a816358ad0c3d27a4
J. Nelson <drumhacker72@gmail.com>
2022-12-20 01:39:21 -0600

    day 20, part 2

--- day20/app/Main.hs
+++ day20/app/Main.hs
@@ -22,9 +22,16 @@ move (x@(_, n) :<| xs) = let (s1, s2) = S.splitAt (n `mod` length xs) xs in x :<
 indexWrapped :: File -> Int -> Int
 indexWrapped xs i = snd $ xs `S.index` (i `mod` length xs)
 
+mix :: File -> File
+mix file = foldl' (\acc n -> move $ focusIdx n acc) file [1..length file]
+
 main :: IO ()
 main = do
     (nums :: [Int]) <- map read . lines <$> readFile "day20.txt"
     let file = S.fromList $ zip [1..] nums
-        file' = focusVal 0 $ foldl' (\acc n -> move $ focusIdx n acc) file [1..length file]
+        file' = focusVal 0 $ mix file
     print $ sum $ map (indexWrapped file') [1000, 2000, 3000]
+
+    let file2 = S.fromList $ zip [1..] $ map (* 811589153) nums
+        file2' = focusVal 0 $ iterate mix file2 !! 10
+    print $ sum $ map (indexWrapped file2') [1000, 2000, 3000]
