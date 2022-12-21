0fde00a9581023a56e162de99582b440b8b51884
J. Nelson <drumhacker72@gmail.com>
2022-12-21 00:18:11 -0600

    day 21, part 2

--- day21/app/Main.hs
+++ day21/app/Main.hs
@@ -54,8 +54,65 @@ eval ms m = case ms M.! m of
     Literal n -> n
     Result op xs -> opFunc op $ bimap (eval ms) (eval ms) xs
 
+data Job2 = Literal2 !Int | Result2 !Operation !(Text, Text) | X2
+    deriving Show
+
+upgrade :: Job -> Job2
+upgrade (Literal a) = Literal2 a
+upgrade (Result op xs) = Result2 op xs
+
+data Expr
+    = LitExpr !Int
+    | AddExpr !Expr !Expr
+    | SubExpr !Expr !Expr
+    | MulExpr !Expr !Expr
+    | DivExpr !Expr !Expr
+    | XExpr
+    deriving Show
+
+addExpr, subExpr, mulExpr, divExpr :: Expr -> Expr -> Expr
+addExpr (LitExpr a) (LitExpr b) = LitExpr (a + b)
+addExpr a b = AddExpr a b
+subExpr (LitExpr a) (LitExpr b) = LitExpr (a - b)
+subExpr a b = SubExpr a b
+mulExpr (LitExpr a) (LitExpr b) = LitExpr (a * b)
+mulExpr a b = MulExpr a b
+divExpr (LitExpr a) (LitExpr b) = LitExpr (a `div` b)
+divExpr a b = DivExpr a b
+
+build :: Map Text Job2 -> Text -> Expr
+build ms m = case ms M.! m of
+    Literal2 n -> LitExpr n
+    Result2 Add (a, b) -> addExpr (build ms a) (build ms b)
+    Result2 Sub (a, b) -> subExpr (build ms a) (build ms b)
+    Result2 Mul (a, b) -> mulExpr (build ms a) (build ms b)
+    Result2 Div (a, b) -> divExpr (build ms a) (build ms b)
+    X2 -> XExpr
+
+solve :: Expr -> Expr -> Int
+solve (LitExpr _) (LitExpr _) = error "unexpected"
+solve a@(LitExpr _) b = solve b a
+solve XExpr (LitExpr z) = z
+solve (AddExpr (LitExpr a) b) (LitExpr z) = solve b (LitExpr $ z - a)
+solve (AddExpr a (LitExpr b)) (LitExpr z) = solve a (LitExpr $ z - b)
+solve (SubExpr (LitExpr a) b) (LitExpr z) = solve b (LitExpr $ a - z)
+solve (SubExpr a (LitExpr b)) (LitExpr z) = solve a (LitExpr $ z + b)
+solve (MulExpr (LitExpr a) b) (LitExpr z) = solve b (LitExpr $ z `div` a)
+solve (MulExpr a (LitExpr b)) (LitExpr z) = solve a (LitExpr $ z `div` b)
+solve (DivExpr (LitExpr a) b) (LitExpr z) = solve b (LitExpr $ a `div` z)
+solve (DivExpr a (LitExpr b)) (LitExpr z) = solve a (LitExpr $ b * z)
+solve _ _ = error "unexpected"
+
 main :: IO ()
 main = do
     contents <- decodeASCII <$> B.readFile "day21.txt"
     let monkeys = M.fromList $ maybe (error "bad input") id $ parseMaybe (sepEndBy1 pMonkeyJob eol) contents
     print $ eval monkeys "root"
+
+    let monkeys2 = M.mapWithKey (\m j -> if m == "humn" then X2 else upgrade j) monkeys
+        (left, right) = case monkeys2 M.! "root" of
+            (Result2 _ xs) -> xs
+            _ -> error "expected root to listen for 2 numbers"
+        e1 = build monkeys2 left
+        e2 = build monkeys2 right
+    print $ solve e1 e2
